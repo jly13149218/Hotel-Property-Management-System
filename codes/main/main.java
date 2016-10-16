3081d2780dad1e47daed3195f3e6ffe093e0bb41
chinalienming <728385437@qq.com>
2016-10-16 15:02:36 +0800

    lienming的模块
    添加 AccountBLService.java  AccountBLService_Driver.java
    AccountBLService_Stub.java  AccoundatabaseServiceMysqlImpl_Stub.java
    在main.java  DataBaseFactory.java 添加内容
    在描述文档中修改表11（添加内容）

--- codes/main/main.java
+++ codes/main/main.java
@@ -3,5 +3,9 @@ public class Client {
         UserInfoBLService  UserInfoBLServiceController = new UserInfoController();
         UserInfoBLService_Driver driver = new UserInfoBLService_Driver();
         driver.drive(UserInfoController);
+
+	AccountBLService accontController = new AccountBLService();
+	AccountBLService_Driver accountBLService_driver= new AccountBLService_Driver(accountController);
+	driver.drive(accontController);	
     }
 }
